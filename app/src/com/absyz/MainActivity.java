e7831484cf784ccc41cfb086c472c199b165f357
gokulrajan <gokulrajanoff@gmail.com>
2018-05-18 19:27:30 +0530

    friday-done changes waiting for Session to expire

--- app/src/com/absyz/MainActivity.java
+++ app/src/com/absyz/MainActivity.java
@@ -31,36 +31,22 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-import com.salesforce.androidsdk.analytics.AnalyticsPublisherService;
 import com.salesforce.androidsdk.app.SalesforceSDKManager;
-import com.salesforce.androidsdk.rest.ApiVersionStrings;
-import com.salesforce.androidsdk.rest.ClientManager;
 import com.salesforce.androidsdk.rest.RestClient;
-import com.salesforce.androidsdk.rest.RestClient.AsyncRequestCallback;
-import com.salesforce.androidsdk.rest.RestRequest;
-import com.salesforce.androidsdk.rest.RestResponse;
-import com.salesforce.androidsdk.smartsync.app.SmartSyncSDKManager;
 import com.salesforce.androidsdk.ui.SalesforceActivity;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
-
-import static android.support.constraint.Constraints.TAG;
 
 /**
  * Main activity
@@ -68,136 +54,133 @@ import static android.support.constraint.Constraints.TAG;
 public class MainActivity extends SalesforceActivity {
 
     private RestClient client;
-	private TextView errorTextView;
-	private String AskToLogout="Try To Login Out And Login Again";
-	private String URL="https://test.salesforce.com/services/oauth2/token";
-	private String Client_ID ="3MVG99S6MzYiT5k9Zi_aoc.dquXJmkC3UIOksfnVzO5qn9KqIJ3KfkOk0WMZr9uqDjeCEEx4bd43jhObsZPkn";
-	private String Client_Secret ="8240518122990865023";
-	private String TAG = "CLIMA";
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		errorTextView = findViewById(R.id.Error);
-		// Setup view
-		setContentView(R.layout.main);
-		Log.d(TAG, "onCreate: Invalidate the current Token");
-	}
-	
-	@Override 
-	public void onResume() {
-		// Hide everything until we are logged in
-		findViewById(R.id.root).setVisibility(View.INVISIBLE);
+    private TextView errorTextView;
+    private String AskToLogout = "Try To Login Out And Login Again";
+    private String URL = "https://test.salesforce.com/services/oauth2/token";
+    private String Client_ID = "3MVG99S6MzYiT5k9Zi_aoc.dquXJmkC3UIOksfnVzO5qn9KqIJ3KfkOk0WMZr9uqDjeCEEx4bd43jhObsZPkn";
+    private String Client_Secret = "8240518122990865023";
+    private String TAG = "CLIMA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        errorTextView = findViewById(R.id.Error);
+        // Setup view
+        setContentView(R.layout.main);
+        Log.d(TAG, "onCreate: Invalidate the current Token");
+    }
+
+    @Override
+    public void onResume() {
+        // Hide everything until we are logged in
+        findViewById(R.id.root).setVisibility(View.INVISIBLE);
 
 
-		
-		super.onResume();
-	}		
-	
-	@Override
-	public void onResume(RestClient client) {
+        super.onResume();
+    }
+
+    @Override
+    public void onResume(RestClient client) {
         // Keeping reference to rest client
         this.client = client;
-		System.out.println("OnResume "+client.getAuthToken());
+        System.out.println("OnResume " + client.getAuthToken());
 
-		//cm.peekRestClient();
-		// Show everything
-		findViewById(R.id.root).setVisibility(View.VISIBLE);
-	}
+        //cm.peekRestClient();
+        // Show everything
+        findViewById(R.id.root).setVisibility(View.VISIBLE);
+    }
 
-	/**
-	 * Called when "Logout" button is clicked. 
-	 * 
-	 * @param v
-	 */
-	public void onLogoutClick(View v) {
-		SalesforceSDKManager.getInstance().logout(this);
-	}
+    /**
+     * Called when "Logout" button is clicked.
+     *
+     * @param v
+     */
+    public void onLogoutClick(View v) {
+        SalesforceSDKManager.getInstance().logout(this);
+    }
 
-	public class RefreshToken extends AsyncTask<String,Void,String>
-	{
+    public void Testing(View view) {
 
-		@Override
-		protected String doInBackground(String... strings) {
+        System.out.println("Old Token " + client.getAuthToken());
+        new RefreshToken().execute("");
+        BeforeOpeningWebView();
+        System.out.println("New Token " + client.getAuthToken());
 
-			RestClient.OAuthRefreshInterceptor oAuth = new RestClient.OAuthRefreshInterceptor(client.getClientInfo(), client.getAuthToken(),client.getOAuthRefreshInterceptor().getAuthTokenProvider());
-			try {
-				oAuth.RefreshTokenManually();
+    }
 
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-	}
-	public void Testing(View view)
-	{
+    public void CheckForConnection(View view) {
+        Log.d(TAG, "\n CheckForConnection: Old Access token" + client.getAuthToken());
+        AsyncHttpClient Aclient = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", client.getRefreshToken());
+        params.add("client_id", Client_ID);
+        params.add("client_secret", Client_Secret);
 
-				System.out.println("Old Token "+client.getAuthToken());
-				new RefreshToken().execute("");
-				BeforeOpeningWebView();
+        Aclient.post(URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 
-				// UI code goes here
-
-		System.out.println("New Token "+client.getAuthToken());
-
-		}
-
-	public void CheckForConnection(View view) {
-        Log.d(TAG, "\n CheckForConnection: Old Access token"+client.getAuthToken());
-		AsyncHttpClient Aclient = new AsyncHttpClient();
-		RequestParams params = new RequestParams();
-		params.add("grant_type","refresh_token");
-		params.add("refresh_token",client.getRefreshToken());
-		params.add("client_id",Client_ID);
-		params.add("client_secret",Client_Secret);
-
-		Aclient.post(URL,params,new JsonHttpResponseHandler(){
-			@Override
-			public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-
-				Log.d("HTTP", "OnSuccess: "+statusCode+"Header"+headers+"Response"+response.toString());
+                Log.d("HTTP", "OnSuccess: " + statusCode + "Header" + headers + "Response" + response.toString());
                 try {
-                    Toast.makeText(getApplicationContext(),"Logging in",Toast.LENGTH_SHORT).show();
-                    String communityUrl= String.valueOf(client.getClientInfo().communityUrl);
-                    System.out.println("community URL"+client.getClientInfo().communityUrl);
-                    Log.d(TAG, "\nonSuccess: New Access Token"+response.get("access_token"));
-                    String url = ""+ communityUrl +"/one/one.app?sid="+ response.get("access_token")+"";
+                    Toast.makeText(getApplicationContext(), "Logging in", Toast.LENGTH_SHORT).show();
+                    String communityUrl = String.valueOf(client.getClientInfo().communityUrl);
+                    System.out.println("community URL" + client.getClientInfo().communityUrl);
+                    Log.d(TAG, "\nonSuccess: New Access Token" + response.get("access_token"));
+                    String url = "" + communityUrl + "/one/one.app?sid=" + response.get("access_token") + "";
                     OpenWebView(url);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-			@Override
-			public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
 
-				Log.d("HTTP", "OnFailure: "+statusCode+"Header"+headers+"Response"+errorResponse.toString()+"Throwbable"+throwable.getMessage());
-			    Toast.makeText(getApplicationContext(),throwable.getMessage(),Toast.LENGTH_SHORT).show();
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+                Log.d("HTTP", "OnFailure: " + statusCode + "Header" + headers + "Response" + errorResponse.toString() + "Throwbable" + throwable.getMessage());
+                Toast.makeText(getApplicationContext(), throwable.getMessage(), Toast.LENGTH_SHORT).show();
                 LoginError();
-			}
+            }
 
-		});
+        });
 
-	}
+    }
 
     private void LoginError() {
-        errorTextView=findViewById(R.id.Error);
+        errorTextView = findViewById(R.id.Error);
         errorTextView.setText(AskToLogout);
     }
-    public void BeforeOpeningWebView()
-	{
-		String communityUrl= String.valueOf(client.getClientInfo().communityUrl);
-		System.out.println("community URL"+client.getClientInfo().communityUrl);
-		Log.d(TAG, "\nonSuccess: New Access Token"+client.getAuthToken());
-		String url = ""+ communityUrl +"/one/one.app?sid="+ client.getAuthToken()+"";
-		OpenWebView(url);
-	}
+
+    public void BeforeOpeningWebView() {
+        String communityUrl = String.valueOf(client.getClientInfo().communityUrl);
+        System.out.println("community URL" + client.getClientInfo().communityUrl);
+        Log.d(TAG, "\nonSuccess: New Access Token" + client.getAuthToken());
+        String url = "" + communityUrl + "/one/one.app?sid=" + client.getAuthToken() + "";
+        OpenWebView(url);
+    }
 
     public void OpenWebView(String url) {
-        Intent OpenWebView = new Intent(this,ConnectWebViewActivity.class);
-        OpenWebView.putExtra("URL",url);
-        Toast.makeText(getApplicationContext(),"Logging in",Toast.LENGTH_SHORT).show();
+        Intent OpenWebView = new Intent(this, ConnectWebViewActivity.class);
+        OpenWebView.putExtra("URL", url);
+        Toast.makeText(getApplicationContext(), "Logging in", Toast.LENGTH_SHORT).show();
         startActivity(OpenWebView);
 
-	}
+    }
+
+    public class RefreshToken extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            RestClient.OAuthRefreshInterceptor oAuth = new RestClient.OAuthRefreshInterceptor(client.getClientInfo(), client.getAuthToken(), client.getOAuthRefreshInterceptor().getAuthTokenProvider());
+            try {
+                oAuth.RefreshTokenManually();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
 }
